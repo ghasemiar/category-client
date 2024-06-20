42f4b44c05f5367ac3ba4377c855d38e402854c3
Reza <ghasemiar82@gmail.com>
2024-06-20 03:35:15 -0700

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <MapComponent class="test" :options="{mapKey:'web.a0be47102c054a1887734fb0b75dc0af',mapType:MapTypes.neshanVectorNight}"/>
+  </div>
+</template>
+<script setup>
+
+import {MapComponent, MapTypes} from '@neshan-maps-platform/mapbox-gl-vue';
+import '@neshan-maps-platform/mapbox-gl-vue/dist/style.css';
+
+</script>
+<style lang="css" scoped>
+.test{
+  height: 400px;
+  width: 600px;
+}
+</style>
